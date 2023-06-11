c8164ac92a40f663c35837425647c879f9f3b06f
slizer98 <erick.m.villa.r@gmail.com>
2023-06-11 08:26:11 -0600

    useTodo agregado

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,22 @@
+
+
+export const todoReducer = (initialState = [], action) => {
+    switch (action.type) {
+        case 'add':
+            return [...initialState, action.payload];
+        case 'remove':
+            return initialState.filter(todo => todo.id !== action.payload)
+        case 'complete':
+            return initialState.map(todo => {
+                if(todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo;
+            })
+        default:
+            return initialState;
+    }
+}
